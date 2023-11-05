b9478a2c886fe713cc21154f3e017bee4967bddc
Selim-Ramdani <selimcodeur@gmail.com>
2023-11-05 14:58:38 +0100

    Project almost finished

---
+++ purgecss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html", "./assets/scripts.js", "./assets/maugallery.js", ""],
+  css: [
+    "./assets/bootstrap/bootstrap.css",
+    "./assets/bootstrap/bootstrap.min.css",
+  ],
+  keyframes: true,
+  output: "./assets/css_purified/",
+  rejected: true,
+  variables: true,
+};
